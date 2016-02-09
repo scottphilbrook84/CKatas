1136f5cb7a2efb4a81bec6417a0207f3c39790cc
sphilbrook84 <ernieballsep@gmail.com>
2016-02-08 22:19:23 -0500

    Inital commit

---
+++ CKatas.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCase(155, Result = -1)]
+    [TestCase(121, Result = 144)]
+    [TestCase(625, Result = 676)]
+    [TestCase(319225, Result = 320356)]
+    [TestCase(15241383936, Result = 15241630849)]
+    public static long FixedTest(long num)
+    {
+        return Kata.FindNextSquare(num);
+    }
+}
---
+++ CKatas/Class1.cs
@@ -0,0 +1,8 @@
+﻿public class Kata
+{
+    public static long FindNextSquare(long num)
+    {
+        // your code here
+        return 0;
+    }
+}
