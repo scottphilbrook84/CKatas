a4d264d0d33bbb6ed12e0713e5dbfd91c43ac276
sphilbrook84 <ernieballsep@gmail.com>
2016-02-08 22:43:10 -0500

    Test

--- CKatas/Class1.cs
+++ CKatas/Class1.cs
@@ -2,7 +2,18 @@
 {
     public static long FindNextSquare(long num)
     {
+
         // your code here
-        return 0;
+        var x = System.Math.Pow(num, 1 / 3);
+        x = System.Math.Round(x);
+        if (System.Math.Pow(x, 3) == num)
+        {
+            return (long)System.Math.Pow(x + 1, 3);
+        }
+        else
+        {
+            return -1;
+
+        }
     }
 }
