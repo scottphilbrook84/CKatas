8c0e4faf1052319128a39a05e33d7e2eb3e87b2d
sphilbrook84 <ernieballsep@gmail.com>
2016-02-10 21:19:07 -0500

    Two new Katas for Today

---
+++ CKatas.Test/HowMany2s.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CKatas.Test
+{
+    [TestClass]
+    public class HowMany2s
+    {
+        [TestMethod]
+        public void FixedTest()
+        {
+            Assert.AreEqual(3, Kata.TwoCount(24));
+            Assert.AreEqual(7, Kata.TwoCount(17280));
+            Assert.AreEqual(1, Kata.TwoCount(222222222));
+            Assert.AreEqual(8, Kata.TwoCount(256));
+            Assert.AreEqual(0, Kata.TwoCount(1));
+            Assert.AreEqual(1, Kata.TwoCount(2));
+            Assert.AreEqual(0, Kata.TwoCount(7));
+        }
+    }
+}
--- CKatas/Katas.cs
+++ CKatas/Katas.cs
@@ -18,8 +18,19 @@ public class Kata
 
     public static double Chain(double input, Func<double, double>[] fs)
     {
-        fs.ToList().ForEach(x=> input = x.Invoke(input));
+        fs.ToList().ForEach(x => input = x.Invoke(input));
         return input;
+    }
+
+    public static int TwoCount(int n)
+    {
+        int numberOf2s = 0;
+        while (n % 2 == 0)
+        {
+            n = n>>1;
+            numberOf2s += 1;
+        }
+        return numberOf2s;
     }
 
 }
