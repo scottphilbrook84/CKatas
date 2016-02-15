09a19476af515238291b051c7b8c8fbb122e0d0a
sphilbrook84 <ernieballsep@gmail.com>
2016-02-14 21:44:28 -0500

    Kata for the day

---
+++ CKatas.Test/TwistedSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CKatas.Test
+{
+    [TestClass]
+    public class TwistedSumTest
+    {
+        [TestMethod]
+        public void When03_Then6()
+        {
+            Assert.AreEqual(6, TwistedSum.Solution(3));
+        }
+        [TestMethod]
+        public void When10_Then46()
+        {
+            Assert.AreEqual(46, TwistedSum.Solution(10));
+        }
+        [TestMethod]
+        public void When11_Then48()
+        {
+            Assert.AreEqual(48, TwistedSum.Solution(11));
+        }
+        [TestMethod]
+        public void When12_Then51()
+        {
+            Assert.AreEqual(51, TwistedSum.Solution(12));
+        }
+        [TestMethod]
+        public void When123_Then1038()
+        {
+            Assert.AreEqual(1038, TwistedSum.Solution(123));
+        }
+
+    }
+}
--- CKatas/Katas.cs
+++ CKatas/Katas.cs
@@ -27,7 +27,7 @@ public class Kata
         int numberOf2s = 0;
         while (n % 2 == 0)
         {
-            n = n>>1;
+            n = n >> 1;
             numberOf2s += 1;
         }
         return numberOf2s;
@@ -40,6 +40,41 @@ public class EmailObfuscator
 
     public static string Obfuscate(string email)
     {
-        return email.Replace("@"," [at] ").Replace("."," [dot] ");
+        return email.Replace("@", " [at] ").Replace(".", " [dot] ");
+    }
+}
+
+public class TwistedSum
+{
+    public static long Solution(long n)
+    {
+        long hundreds = n / 100;
+        long sum = 900 * hundreds;
+
+        for (long start = 1; start < hundreds; start ++)
+        {
+            long digit = start;
+            long digitsum = 0;
+            while (digit > 0)
+            {
+                digitsum += digit % 10;
+                digit = digit / 10;
+            }
+            sum += 100 * digitsum;
+        }
+
+        long remainder = n % 100;
+        for (long loopindex = (n - remainder); loopindex <= n; loopindex++)
+        {
+            long digit = loopindex;
+            while (digit > 0)
+            {
+                sum += digit % 10;
+                digit = digit / 10;
+            }
+        }
+
+
+        return sum;
     }
 }
